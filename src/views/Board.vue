557aaeff984f85cfbcba6c51dc968d861a714be1
Douglas Felchilcher <douglasfelc@gmail.com>
2023-07-05 22:40:44 -0300

    Iniciado painel

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,101 +1,35 @@
 <template>
   <CRow>
-    <CCol :xs="12">
-      <CCard class="mb-4">
-        <CCardHeader>
-          <strong>Card</strong> <small>Grid cards</small>
-        </CCardHeader>
+    <CCol v-for="(status, key) in statusCollection" :key="key">
+      <CCard>
         <CCardBody>
-          <DocsExample href="components/card.html/#grid-cards">
-            <CRow :xs="{ cols: 1, gutter: 4 }" :md="{ cols: 3 }">
-              <CCol xs>
-                <CCard>
-                  <CCardImage orientation="top" :src="VueImg" />
-                  <CCardBody>
-                    <CCardTitle>Não iniciado</CCardTitle>
-                    <CCardText>
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </CCardText>
-                  </CCardBody>
-                  <CCardFooter>
-                    <small class="text-medium-emphasis"
-                      >Last updated 3 mins ago</small
-                    >
-                  </CCardFooter>
-                </CCard>
-              </CCol>
-              <CCol xs>
-                <CCard>
-                  <CCardImage orientation="top" :src="VueImg" />
-                  <CCardBody>
-                    <CCardTitle>Em progresso</CCardTitle>
-                    <CCardText>
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </CCardText>
-                  </CCardBody>
-                  <CCardFooter>
-                    <small class="text-medium-emphasis"
-                      >Last updated 3 mins ago</small
-                    >
-                  </CCardFooter>
-                </CCard>
-              </CCol>
-              <CCol xs>
-                <CCard>
-                  <CCardImage orientation="top" :src="VueImg" />
-                  <CCardBody>
-                    <CCardTitle>Concluído</CCardTitle>
-                    <CCardText>
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </CCardText>
-                  </CCardBody>
-                  <CCardFooter>
-                    <small class="text-medium-emphasis"
-                      >Last updated 3 mins ago</small
-                    >
-                  </CCardFooter>
-                </CCard>
-              </CCol>
-              <CCol xs>
-                <CCard>
-                  <CCardImage orientation="top" :src="VueImg" />
-                  <CCardBody>
-                    <CCardTitle>Card title</CCardTitle>
-                    <CCardText>
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </CCardText>
-                  </CCardBody>
-                  <CCardFooter>
-                    <small class="text-medium-emphasis"
-                      >Last updated 3 mins ago</small
-                    >
-                  </CCardFooter>
-                </CCard>
-              </CCol>
-            </CRow>
-          </DocsExample>
+          <CCardTitle>{{ status.name }}</CCardTitle>
+          <CCardText> Cards aqui dentro </CCardText>
         </CCardBody>
+        <CCardFooter>
+          <small class="text-medium-emphasis">Last updated 3 mins ago</small>
+        </CCardFooter>
       </CCard>
     </CCol>
   </CRow>
 </template>
 
 <script>
-import VueImg from '@/assets/images/vue.jpg'
 export default {
-  name: 'Cards',
-  setup() {
+  name: 'Board',
+  data() {
     return {
-      VueImg,
+      statusCollection: [],
     }
+  },
+  mounted() {
+    this.getStatus()
+  },
+  methods: {
+    getStatus() {
+      const statusCollection = localStorage.getItem('statusCollection')
+      if (statusCollection) this.statusCollection = JSON.parse(statusCollection)
+    },
   },
 }
 </script>
